305cddd774e95a93b1a2a885170ed51ededa0517
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-05-12 11:42:39 +0100

    high pass filter

--- Week2/Day 5/Practice/Array Challenges/ArrayChallenges.js
+++ Week2/Day 5/Practice/Array Challenges/ArrayChallenges.js
@@ -17,3 +17,16 @@ alwaysHungry([3.14, "food", "pie", true, "food"]);
 
 var hungry = alwaysHungry([4, 1, 5, 7, 2]);
 console.log(hungry)
+//Question 2: High Pass Filter
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (let i = 0; i < arr.length; i ++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); 
+
