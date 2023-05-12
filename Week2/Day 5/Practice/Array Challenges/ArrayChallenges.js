f50c18be031be8df54ac33337fdc1922276f5929
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-05-12 11:43:15 +0100

    better than average

--- Week2/Day 5/Practice/Array Challenges/ArrayChallenges.js
+++ Week2/Day 5/Practice/Array Challenges/ArrayChallenges.js
@@ -29,4 +29,24 @@ function highPass(arr, cutoff) {
 }
 var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
 console.log(result); 
+//Question 3: Better than average
+
+function betterThanAverage(arr) {
+    var sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = Math.round(sum + arr[i]/arr.length);
+    }
+    var count = 0
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > sum) {
+            count++
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); 
+
+
+
 
